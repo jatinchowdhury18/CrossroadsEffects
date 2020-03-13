7e2a81e13798f84985d1fd313662433d6cca9fa5
Jatin Chowdhury <jchowdhu@stanford.edu>
2020-03-13 14:12:31 -0700

    Update full test to check for build artifacts

--- run_tests.sh
+++ run_tests.sh
@@ -15,9 +15,23 @@ elif [[ $1 == "--params" ]]; then
 fi
 
 if [ -z "$1" ]; then
+    # test full system
     echo "Testing Crossroads"
     $PY crossroads.py --name=Gain audio_files/drums.wav audio_files/gain.wav
+
+    echo "Checking artifacts..."
+    name=Gain
+    [ -d $name ] || { echo "Error: Output folder not found!" && exit 1; }
+    [ -d $name/gen0_0 -a -d $name/gen0_1 -a -d $name/gen0_2 ] || { echo "Error: Generation output folders not found!" && exit 1; }
+    [ -d $name/gen_final ] || { echo "Error: Final output folder not found!" && exit 1; }
+
+    tfile=$name/gen_final/$name
+    [ -e $tfile.dsp ] || { echo "Error: Output Faust script file not found!" && exit 1; }
+    [ -e $tfile.dll -o -e $tfile.vst -o -e $tfile.so ] || { echo "Error: Output VST file not found!" && exit 1; }
+    [ -e $name/gen_final/svgs/process.svg ] || { echo "Error: Output SVG not found" && exit 1; }
+    echo "SUCCESS"
 else
+    # run test scripts
     for f in $pattern
     do
 
